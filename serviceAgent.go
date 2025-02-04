375c4024eb41f34e678bc8c25f3e0b106807b18a
springrain <chunanyong@163.com>
2025-02-04 20:06:43 +0800

    增加Agent界面

---
+++ serviceAgent.go
@@ -0,0 +1,32 @@
+// Copyright (c) 2025 minrag Authors.
+//
+// This file is part of minrag.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"context"
+
+	"gitee.com/chunanyong/zorm"
+)
+
+// findAllAgentList 查询所有的智能体
+func findAllAgentList(ctx context.Context) ([]Agent, error) {
+	finder := zorm.NewSelectFinder(tableAgentName).Append("order by sortNo desc")
+	list := make([]Agent, 0)
+	err := zorm.Query(ctx, finder, &list, nil)
+	return list, err
+}
