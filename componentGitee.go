d94fcc79c7ada6ea92028c071fd4a1d1668167a9
springrain <chunanyong@163.com>
2025-02-14 20:29:08 +0800

    修改注释

--- componentGitee.go
+++ componentGitee.go
@@ -1,6 +1,6 @@
-// Copyright (c) 2025 minrag Authors.
+// Copyright (c) 2025 minRAG Authors.
 //
-// This file is part of minrag.
+// This file is part of minRAG.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -13,7 +13,7 @@
 // GNU Affero General Public License for more details.
 //
 // You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// along with this program.  If not, see <https://www.gnu.org/licenses>.
 
 // 百度千帆平台,主要适配Reranker模型
 package main
