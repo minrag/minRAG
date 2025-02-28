3667e2a93047ecbaff1ae8ae86c44bcdb5e27c12
springrain <chunanyong@163.com>
2025-02-28 12:02:18 +0800

    增加WebScraper组件,实现网络爬虫

--- componentOpenAI_test.go
+++ componentOpenAI_test.go
@@ -14,7 +14,24 @@ func TestHtmlCleaner(t *testing.T) {
 	input := make(map[string]interface{}, 0)
 	document := &Document{Markdown: mk}
 	input["document"] = document
-	err := hc.Run(context.Background(), input)
+	ctx := context.Background()
+	hc.Initialization(ctx, input)
+	err := hc.Run(ctx, input)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(document.Markdown)
+}
+
+func TestWebScraper(t *testing.T) {
+	ws := WebScraper{}
+	input := make(map[string]interface{}, 0)
+	document := &Document{Markdown: ""}
+	input["document"] = document
+	ws.WebURL = "https://baidu.com"
+	ctx := context.Background()
+	ws.Initialization(ctx, input)
+	err := ws.Run(ctx, input)
 	if err != nil {
 		t.Fatal(err)
 	}
