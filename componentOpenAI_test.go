134a3376b00141642560563d1529ec4d64f40904
springrain <chunanyong@163.com>
2025-03-04 18:06:42 +0800

    修改爬虫细节

--- componentOpenAI_test.go
+++ componentOpenAI_test.go
@@ -24,16 +24,20 @@ func TestHtmlCleaner(t *testing.T) {
 }
 
 func TestWebScraper(t *testing.T) {
-	ws := WebScraper{}
+	ws := WebScraper{QuerySelector: []string{"#s-top-left"}}
+	ws.Depth = 2
 	input := make(map[string]interface{}, 0)
 	document := &Document{Markdown: ""}
 	input["document"] = document
-	ws.WebURL = "https://baidu.com"
+	ws.WebURL = "https://www.baidu.com"
 	ctx := context.Background()
 	ws.Initialization(ctx, input)
-	err := ws.Run(ctx, input)
+	herf, err := ws.FetchPage(ctx, document, input)
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println(document.Markdown)
+	fmt.Println(document)
+	fmt.Println("-------------------")
+	fmt.Println(herf)
+
 }
